3795933b0fb4ab5c619b7459ea70877e1a40afbb
Tonystli18 <34615551+Tonystli18@users.noreply.github.com>
2020-02-12 15:32:39 -0800

    First Commit.

---
+++ resources/views/faq.blade.php
@@ -0,0 +1,21 @@
+@extends('layout')
+
+@section('content')
+
+<section class="">
+    <div id="app" class="container">
+        <div class="w-3/5 m-auto generic-content">
+            <header class="mb-10 text-center">
+                <h1 class="text-3xl mb-2">FAQ</h1>
+                <p> It's Ok, from time to time, we all have questions.</p>
+            </header>
+
+            @foreach ($questions as $question)
+            <question :question="{{ json_encode($question) }}"></question>
+
+            @endforeach
+        </div>
+    </div>
+</section>
+
+@stop

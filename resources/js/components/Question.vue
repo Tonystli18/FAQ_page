5aacfeb2ffc3d55ddd982c8959e036415fcf361c
Tonystli18 <34615551+Tonystli18@users.noreply.github.com>
2020-02-13 16:08:25 -0800

    Create Vue component Question.vue and Views.

---
+++ resources/js/components/Question.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="mb-8 flex">
+    <div class="mr-4">
+      <span
+        class="border border-solid border-gray-700 rounded-full w-6 h-6 flex items-center justify-center text-xs text-gray-700 font-bold leading-none cursor-pointer"
+        @click="isOpen = !isOpen"
+      >
+        <template v-if="isOpen">
+          <!-- {{ isOpen ? ' &#10005;' : '&plus;'}} -->
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="12"
+            height="16"
+            viewBox="0 0 12 16"
+            class="fill-current"
+          >
+            <path
+              d="M7.48 8l3.75 3.75-1.48 1.48L6 9.48l-3.75 3.75-1.48-1.48L4.52 8 .77 4.25l1.48-1.48L6 6.52l3.75-3.75 1.48 1.48z"
+            />
+          </svg>
+        </template>
+        <template v-else="isOpen">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="12"
+            height="16"
+            viewBox="0 0 12 16"
+            class="fill-current"
+          >
+            <path
+              d="M12 9H7v5H5V9H0V7h5V2h2v5h5z"
+            />
+          </svg>
+        </template>
+      </span>
+    </div>
+    <div class="leading-loose">
+      <h3
+        v-text="question.question"
+        class="font-bold text-2xl mb-3 text-black leading-none cursor-pointer"
+        @click="isOpen = !isOpen"
+      ></h3>
+
+      <div v-html="question.answer" v-show="isOpen"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["question"],
+
+  data() {
+    return {
+      isOpen: false
+    };
+  }
+};
+</script>
